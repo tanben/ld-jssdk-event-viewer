13b3fc3c2fe1728ef5fff9df53c7bafa3f552c97
btan <>
2022-02-14 09:53:56 -0500

    update UI and event collection logic

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # ld-jssdk-event-viewer
 Launchdarkly JS Client SDK event viewer Chrome extension
 
-![img](img/screen1.jpg)
+## Capture and validate Conversion Metrics
+
+
+![img](img/screen2.jpg)
+## Capture Goals, Feature Flags and Events
+![img](img/screen3.jpg)
 
 # Installation
-1. Clone the repo and follow the instruction [here](https://developer.chrome.com/docs/extensions/mv3/getstarted/#unpacked) on loading an unpacked  Chrome extension.
+1. Clone [this](https://github.com/tanben/ld-jssdk-event-viewer) repo and follow the instruction [here](https://developer.chrome.com/docs/extensions/mv3/getstarted/#unpacked) on loading an unpacked  Chrome extension.
 
 2. Navigate to your page and [Open the Chrome dev-tools](https://launchdarkly.atlassian.net/wiki/spaces/~591637646/pages/edit-v2/2025359513#). 
 
-3. Select Launchdpad from the tab list.
+3. Select **Launchpad** from the tab list.
 4. Reload your page.
 
 
-
-# Dependencies
-* [Ace Editor](https://ace.c9.io/) - packaged with the ACE library [here](https://github.com/ajaxorg/ace-builds/tree/master/src-min) in ./src-min. Copyright notice [here](https://github.com/ajaxorg/ace/blob/master/LICENSE)
--- panel.js
+++ panel.js
@@ -1,4 +1,16 @@
-var extensionGlobals = {};
+var extensionGlobals = {
+    logEditor: {
+        insert: (msg) => {
+            let ele = document.querySelector('textArea#networkDetails');
+            ele.value += `\n`;
+            ele.value += msg;
+        },
+        setValue: (msg) => {
+            let ele = document.querySelector('textArea#networkDetails');
+            ele.value = msg;
+        }
+    }
+};
 
 main();
 
@@ -6,54 +18,28 @@ main();
 //------------
 
 function main() {
-    
-
-    extensionGlobals.logEditor = initializeAceEditor({
-        containerId: "logsContainer",
-        foldCssSelector: "#logs-container>span.fold",
-        unfoldCssSelector: "#logs-container>span.unfold"
-    }, "ace/theme/monokai");
-
-
-    extensionGlobals.flagVariationEditor = initializeAceEditor({
-        containerId: "flagVariationContainer",
-        foldCssSelector: "#evals-container>span.fold",
-        unfoldCssSelector: "#evals-container>span.unfold"
-    });
-
-    extensionGlobals.flagGoalsEditor = initializeAceEditor({
-        containerId: "flagGoalsContainer",
-        foldCssSelector: "#goals-container>span.fold",
-        unfoldCssSelector: "#goals-container>span.unfold"
-    });
-
-    extensionGlobals.experimentsEditor = initializeAceEditor({
-        containerId: "experimentsContainer",
-        foldCssSelector: "#experiments-container>span.fold",
-        unfoldCssSelector: "#experiments-container>span.unfold"
-    });
-
 
     chrome.devtools.network.onRequestFinished.addListener(goalsHandler);
+    chrome.devtools.network.onRequestFinished.addListener(logNetwork);
     chrome.devtools.network.onRequestFinished.addListener(eventsHandler);
     chrome.devtools.network.onRequestFinished.addListener(evalxHandler);
-    
 
-    
-    chrome.devtools.network.onNavigated.addListener(() =>{
-        extensionGlobals.logEditor.setValue("");
-        extensionGlobals.flagVariationEditor.setValue("");
-        extensionGlobals.flagGoalsEditor.setValue("");
-        extensionGlobals.experimentsEditor.setValue("");
-        
-        let typeCounters= window.document.querySelectorAll('.typeCounterValue');
-        typeCounters.forEach(counter=>(counter.textContent=0));
-
-        return contentTypes = {};
-    });
+    chrome.devtools.network.onNavigated.addListener(onNavHandler);
 }
+
+function onNavHandler() {
+    document.querySelectorAll('.metric').forEach(ele => ele.remove());
+
+    let typeCounters = window.document.querySelectorAll('span.type-counter');
+    typeCounters.forEach(counter => (counter.textContent = 0));
+
+    extensionGlobals.logEditor.setValue("");
+    document.querySelector('.experiments-details').value = null;
+    document.querySelector('.featureflags-details').value = null;
+}
+
 function evalxHandler(request) {
-    
+
     if (!request.request.url.includes("launchdarkly.com")) {
         return
     }
@@ -62,18 +48,17 @@ function evalxHandler(request) {
         return;
     }
 
-    
+
     request.getContent((body) => {
         let bodyObj = JSON.parse(body);
-        let bodyStrfy = JSON.stringify(bodyObj, null, 2);
 
         if (bodyObj.length == 0) {
-            extensionGlobals.flagVariationEditor.setValue("No Feature Flags detected.");
-            extensionGlobals.flagVariationEditor.getSession().selection.clearSelection();
             return;
         }
-        extensionGlobals.flagVariationEditor.setValue(bodyStrfy);
-        extensionGlobals.flagVariationEditor.getSession().selection.clearSelection();
+        let bodyStrfy = JSON.stringify(bodyObj, null, 4);
+
+        let ffTextArea = document.querySelector('.featureflags-details');
+        ffTextArea.value = bodyStrfy;
 
         extensionGlobals.logEditor.insert("\n");
         extensionGlobals.logEditor.insert("======== RECEIVE EVENT START ========\n");
@@ -82,9 +67,9 @@ function evalxHandler(request) {
         extensionGlobals.logEditor.insert(bodyStrfy);
         extensionGlobals.logEditor.insert("\n");
         extensionGlobals.logEditor.insert("======== RECEIVE EVENT END   ========\n");
-        extensionGlobals.logEditor.getSession().selection.clearSelection();
     });
 }
+
 function eventsHandler(request) {
     if (!request.request.url.includes("launchdarkly.com")) {
         return
@@ -99,51 +84,80 @@ function eventsHandler(request) {
     extensionGlobals.logEditor.insert("======== SENT EVENT START ========\n");
     extensionGlobals.logEditor.insert(`${request.request.method} url[${request.request.url}]`);
     extensionGlobals.logEditor.insert("\n");
-    extensionGlobals.logEditor.insert(JSON.stringify(events, null, 2));
+    extensionGlobals.logEditor.insert(JSON.stringify(events, null, 4));
     extensionGlobals.logEditor.insert("\n");
     extensionGlobals.logEditor.insert("======== SENT EVENT END   ========\n");
-    extensionGlobals.logEditor.getSession().selection.clearSelection();
-    
-    let eventTypeCounts= countEventTypes(events);
+
+    let eventTypeCounts = countEventTypes(events);
     updateTypeCounters(eventTypeCounts);
 }
 
-function updateTypeCounters( eventTypeCounts ){
-    Object.keys(eventTypeCounts).forEach( key=>{
-        let eleId= `${key}-value`;
+function updateTypeCounters(eventTypeCounts) {
+    Object.keys(eventTypeCounts).forEach(key => {
+        let eleId = `${key}-value`;
         let ele = window.document.querySelector(`#${eleId}`);
-        if (!ele){
+        if (!ele) {
             log(`updateTypeCounters(): eleId=${eleId} NOT FOUND!`);
             return;
         }
-        ele.textContent= parseInt(ele.textContent) + eventTypeCounts[key];
+        ele.textContent = parseInt(ele.textContent) + eventTypeCounts[key];
     })
 }
 
-function updateExperimentsCounter(count){
+function updateExperimentsCounter(count) {
     let ele = window.document.querySelector("#experiments-value");
-    ele.textContent= parseInt(ele.textContent) + count;
+    ele.textContent = parseInt(ele.textContent) + count;
 }
 
-function countEventTypes( events ){
-    
-    return events.reduce ( (acc, curr)=>{
-        let {custom, click, identify, feature} = acc;
-        let {kind} = curr;
-        switch (kind){
-            case 'identify': identify++; break;
-            case 'custom': custom++; break;
-            case 'click': click++; break;
-            case 'feature': feature++; break;
+function countEventTypes(events) {
+
+    return events.reduce((acc, curr) => {
+        let {
+            custom,
+            click,
+            identify,
+            feature
+        } = acc;
+        let {
+            kind
+        } = curr;
+
+        switch (kind) {
+            case 'identify':
+                identify++;
+                break;
+            case 'custom':
+                custom++;
+                break;
+            case 'click':
+                click++;
+                break;
+            case 'feature':
+                feature++;
+                break;
+            case 'summary':
+                feature = Object.keys(curr.features).length;
+                break;
         }
-        return {identify, custom, click, feature};
-    }, {custom:0, click:0, identify:0, feature:0});
+        // log (`countEventTypes=${kind}`);
+        return {
+            identify,
+            custom,
+            click,
+            feature
+        };
+    }, {
+        custom: 0,
+        click: 0,
+        identify: 0,
+        feature: 0
+    });
 }
 
 
 
 function goalsHandler(request) {
-    
+
     if (!request.request.url.includes("launchdarkly.com")) {
         return
     }
@@ -152,198 +166,208 @@ function goalsHandler(request) {
     }
     request.getContent((body) => {
         let bodyObj = JSON.parse(body);
-        let bodyStrfy = JSON.stringify(bodyObj, null, 2);
+        let bodyStrfy = JSON.stringify(bodyObj, null, 4);
 
         if (bodyObj.length == 0) {
-            extensionGlobals.flagGoalsEditor.setValue("No Goals detected.");
-            extensionGlobals.flagGoalsEditor.getSession().selection.clearSelection();
-
-            extensionGlobals.experimentsEditor.setValue("No metric matching URL and Target Selector");
-            extensionGlobals.experimentsEditor.getSession().selection.clearSelection();
-
             return;
         }
-        extensionGlobals.flagGoalsEditor.setValue(bodyStrfy);
-        extensionGlobals.flagGoalsEditor.getSession().selection.clearSelection();
 
-        Promise.allSettled([    evalInspectPage('window.location.href'),
-                        evalInspectPage('window.location.search'),
-                        evalInspectPage('window.location.hash')])
-        .then( (results)=>{
-            let [winHref, winSearch, winHash] = results;
-            processGoals(bodyObj, winHref.value[0], winSearch.value[0], winHash.value[0]);
-            
-        })
+        Promise.allSettled([evalInspectPage('window.location.href'),
+                evalInspectPage('window.location.search'),
+                evalInspectPage('window.location.hash')
+            ])
+            .then((results) => {
+                let [winHref, winSearch, winHash] = results;
+                processGoals(bodyObj, winHref.value[0], winSearch.value[0], winHash.value[0]);
+            })
     });
-   
+
 }
 
-function initializeAceEditor({
-    containerId,
-    foldCssSelector,
-    unfoldCssSelector
-}, theme) {
-
-    let aceEditor = ace.edit(containerId);
-    aceEditor.getSession().setMode("ace/mode/json");
-    aceEditor.getSession().setUseWrapMode(true);
-    aceEditor.getSession().setUseWorker(false);
-    aceEditor.setReadOnly(true);
-    if (theme) {
-        aceEditor.setTheme(theme);
-    }
-
-    let foldEle = window.document.querySelector(foldCssSelector);
-    let unfoldEle = window.document.querySelector(unfoldCssSelector);
-
-    if (!foldEle.onclick) {
-        foldEle.onclick = function () {
-
-            aceEditor.getSession().foldAll();
-        }
-    }
-    if (!unfoldEle.onclick) {
-        unfoldEle.onclick = function () {
-            aceEditor.getSession().unfold();
-        }
-    }
-    return aceEditor
-}
-
-
-
-
-
-function setExperimentsContainer(kv) {
-    if (!kv || kv && kv.length == 0) {
-        extensionGlobals.experimentsEditor.setValue("No metric matching URL and Target Selector");
-        extensionGlobals.experimentsEditor.getSession().selection.clearSelection();
-
+function updateConversionMetricsTable(goals) {
+    if (!goals || goals && goals.length == 0) {
         return;
     }
 
+    let goalTextArea = document.querySelector('.experiments-details');
+    goalTextArea.value = JSON.stringify(goals, null, 4);
 
-    extensionGlobals.experimentsEditor.insert("+---------------+--------------------------+------+--------+\n");
-    extensionGlobals.experimentsEditor.insert("|      Goal     |   Goal Key               |  URL | Target |\n");
-    extensionGlobals.experimentsEditor.insert("+===============+==========================+======+========+\n");
+    const rowDiv = document.createElement("div");
+    rowDiv.className = 'table-row metric';
+    let goalsMapped = goals.map(({
+        kind = '',
+        key = '',
+        selector,
+        urlMatch = false,
+        targetMatch = 'N/A',
+        urls
+    }) => ({
+        enabled: (urlMatch && (targetMatch == 'N/A' ? true : targetMatch)),
+        kind,
+        key,
+        urlMatch,
+        targetMatch,
+        urls,
+        selector
+    }));
+    goalsMapped.forEach((goal) => {
+        for (key in goal) {
+            if (key === 'urls' || key === 'selector') {
+                continue;
+            }
 
-    (kv || []).forEach(metric => {
-        let {
-            kind,
-            key,
-            urlMatch,
-            targetMatch
-        } = metric;
+            let cell = document.createElement("div");
+            cell.className = "table-cell";
 
-
-        extensionGlobals.experimentsEditor.insert(`|${kind.padEnd(15)}|${key.padEnd(26)}|${urlMatch.toString().padEnd(6)}|${targetMatch.toString().padEnd(8)}|\n`)
-        extensionGlobals.experimentsEditor.insert("+---------------+--------------------------+------+--------+\n");
+            switch (key) {
+                case "enabled":
+                    cell.className += (goal[key] == true) ? " metric-enabled" : " metric-disabled";
+                    cell.textContent = "";
+                    break;
+                default:
+                    cell.textContent = goal[key];
+                    break;
+            }
+            rowDiv.appendChild(cell);
+        }
     })
-
-
-    extensionGlobals.experimentsEditor.getSession().selection.clearSelection();
+    let containerHeaderEle = document.querySelector('#conversionMetricsContainer > div.table-row')
+    containerHeaderEle.appendChild(rowDiv);
 }
+
 
 function processGoals(goals, locationHref, search, hash) {
 
-    let goalsFiltered = goals.filter(({
-        urls
-    }) => {
-        let matchedUrl = urls.filter(url => doesUrlMatch(url, locationHref, search, hash));
-        return (matchedUrl && matchedUrl.length > 0);
-    })
-    // log(`goalsfiltered=${JSON.stringify(goalsFiltered)}`);
-    if(goalsFiltered.length==0){
-        setExperimentsContainer([]);
+    if (goals.length == 0) {
+        updateConversionMetricsTable([]);
         return;
     }
     let tasks = [];
-    let taskKeys=[];
-    let taskKinds=[];
-    let taskSelectors=[];
-    goalsFiltered.forEach( ({kind, key, selector}) =>{
-        let code=`window.document.querySelector("${selector}")`;
-        taskKinds.push(kind);
-        taskKeys.push(key);
-        taskSelectors.push(selector);
-        tasks.push( evalInspectPage (code));
-
+    goals.forEach(({
+        selector
+    }) => {
+        let code = `window.document.querySelector("${selector}")`;
+        tasks.push(evalInspectPage(code));
     })
     Promise.allSettled(tasks)
-    .then(results=>{
-        
-        let matchedList = [];
-        results.forEach ((result, idx)=>
-        {
-            let entry={
-                kind: taskKinds[idx],
-                key: taskKeys[idx],
-                selector: taskSelectors[idx],
-                urlMatch: true,
-                targetMatch: (result.value[0] && result.value[0] != null)?true:false
-            };
-            entry.targetMatch = (entry.kind === 'pageview')?"N/A": entry.targetMatch;
-            matchedList.push(entry);
-            // logJSON(JSON.stringify(entry,null,2));
-        });
-        setExperimentsContainer(matchedList);
-        let enabledExperiments = matchedList.filter( ({urlMatch, targetMatch})=> (urlMatch==true && (targetMatch == true || targetMatch == "N/A")));
-        updateExperimentsCounter(enabledExperiments.length);
+        .then(results => {
+            let collection = [];
+            results.forEach((result, idx) => {
+                let {
+                    kind,
+                    key,
+                    selector,
+                    urls
+                } = goals[idx];
+                let matchedUrl = urls.filter(url => doesUrlMatch(url, locationHref, search, hash));
+                let urlMatch = (matchedUrl && matchedUrl.length > 0);
 
-    });
+                let entry = {
+                    kind,
+                    key,
+                    selector,
+                    urlMatch,
+                    targetMatch: (result.value[0] && result.value[0] != null) ? true : false,
+                    urls
+
+                };
+                entry.targetMatch = (entry.kind === 'pageview') ? "N/A" : entry.targetMatch;
+                collection.push(entry);
+            });
+            updateConversionMetricsTable(collection);
+            let enabledExperiments = collection.filter(({
+                urlMatch,
+                targetMatch
+            }) => (urlMatch == true && (targetMatch == true || targetMatch == "N/A")));
+            updateExperimentsCounter(enabledExperiments.length);
+
+        });
 }
 
 
-function logInspectedWindow(msg){
-     chrome.devtools.inspectedWindow.eval(
+function logInspectedWindow(msg) {
+    chrome.devtools.inspectedWindow.eval(
         `console.log('${msg}');`
     )
 }
 
-function evalInspectPage( code ){
-    return new Promise (resolve=>{
-        chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, {code}, function(result){
+function evalInspectPage(code) {
+    return new Promise(resolve => {
+        chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, {
+            code
+        }, function (result) {
             // log("executeScript: Result Start----");
             // log(JSON.stringify(code,null,2));
             // log (JSON.stringify(result));
             // log("executeScript: Result End----")
 
             resolve(result);
-            
+
         });
     })
 }
 
-function logJSON(jsonObj){
-
-    let code={
-        code:`console.log(${jsonObj})`
-    };
-//     chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, code, function(result){
-//         log(`log: result=${result}`)
-//     });
-    chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, code);
-}
-
-function log(msg){
-
-    let code={
-        code:`console.log('${msg}')`
-    };
-//     chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, code, function(result){
-//         log(`log: result=${result}`)
-//     });
-    chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, code);
-}
-
-function hideToggle(){
-
-    let container = this.event.target.parentElement.querySelector(':scope  div.container');
-    if (!container.offsetParent){
-        container.style="display:block";
-    }else{
-        container.style="display:none";
+function logNetwork(request) {
+    if (!request.request.url.includes("launchdarkly.com")) {
+        return
     }
-    
+    if (!request.request.url.includes("/events/bulk/") || !request.request.url.includes("/sdk/eval")) {
+        return;
+    }
+    new Promise(resolve => {
+        switch (request.request.method) {
+            case "POST":
+                let data = (request.request.postData) ? request.request.postData.text : null;
+                resolve(data);
+                break;
+            case "GET":
+                request.getContent((body) => {
+                    // log(`GET ${request.request.url} ${body}`)
+                    if (!body || body && JSON.parse(body).length == 0) {
+                        return resolve(null);
+                    }
+                    resolve(body);
+                });
+                break;
+            default:
+                resolve(null);
+                break;
+        }
+    }).then(data => {
+        if (!data) {
+            return;
+        }
+
+        extensionGlobals.logEditor.insert("\n");
+        extensionGlobals.logEditor.insert("======== EVENT START ========\n");
+        extensionGlobals.logEditor.insert(`Method: [${request.request.method}] URL: [${request.request.url}]`);
+        extensionGlobals.logEditor.insert("\n");
+        extensionGlobals.logEditor.insert(JSON.stringify(data, null, 4));
+        extensionGlobals.logEditor.insert("\n");
+        extensionGlobals.logEditor.insert("======== EVENT END   ========\n");
+
+    })
+}
+
+function log(msg) {
+
+    let code = {
+        code: `console.log('${msg}')`
+    };
+    //     chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, code, function(result){
+    //         log(`log: result=${result}`)
+    //     });
+    chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, code);
+}
+
+function toggle() {
+
+    let container = this.parentElement.querySelector(':scope  div.container');
+    if (!container.offsetParent) {
+        container.style = "display:block";
+    } else {
+        container.style = "display:none";
+    }
+
 
 }
